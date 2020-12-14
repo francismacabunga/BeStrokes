513e960bb0c649e21549ace826284d30a873c8df
Francis <francisnormanmacabunga@gmail.com>
2020-12-14 22:14:59 +0800

    Bugfix

--- BeStrokes/Controllers/Users/HomeViewController.swift
+++ BeStrokes/Controllers/Users/HomeViewController.swift
@@ -197,7 +197,7 @@ class HomeViewController: UIViewController {
                 }
             }
             DispatchQueue.main.async {
-
+                
                 featuredCollectionView.reloadData()
             }
         }
