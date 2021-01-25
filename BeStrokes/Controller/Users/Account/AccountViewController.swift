af695f2a19c2f93623a89d5b724677e1523c7929
Francis <francisnormanmacabunga@gmail.com>
2021-01-26 00:41:18 +0800

    MVVM refactor

--- BeStrokes/Controller/Users/Account/AccountViewController.swift
+++ BeStrokes/Controller/Users/Account/AccountViewController.swift
@@ -89,10 +89,14 @@ class AccountViewController: UIViewController {
         }
     }
     
-    func showErrorFetchingAlert(using errorMessage: String) {
-        let alert = UIAlertController(title: Strings.homeAlertTitle, message: errorMessage, preferredStyle: .alert)
+    func showErrorFetchingAlert(usingError error: Bool, withErrorMessage: Error? = nil, withCustomizedString: String? = nil) {
+        var alert = UIAlertController()
+        if error {
+            alert = UIAlertController(title: Strings.homeAlertTitle, message: withErrorMessage?.localizedDescription, preferredStyle: .alert)
+        } else {
+            alert = UIAlertController(title: Strings.homeAlertTitle, message: withCustomizedString, preferredStyle: .alert)
+        }
         let tryAgainAction = UIAlertAction(title: Strings.homeAlert1Action, style: .default) { [self] (alertAction) in
-            getSignedInUserData()
             dismiss(animated: true)
         }
         alert.addAction(tryAgainAction)
@@ -101,11 +105,8 @@ class AccountViewController: UIViewController {
     
     func showNoSignedInUserAlert() {
         let alert = UIAlertController(title: Strings.homeAlertTitle, message: Strings.homeAlertMessage, preferredStyle: .alert)
-        let dismissAction = UIAlertAction(title: Strings.homeAlert2Action, style: .default) { (alertAction) in
-            let storyboard = UIStoryboard(name: Strings.mainStoryboard, bundle: nil)
-            let landingVC = storyboard.instantiateViewController(identifier: Strings.landingVC)
-            self.view.window?.rootViewController = landingVC
-            self.view.window?.makeKeyAndVisible()
+        let dismissAction = UIAlertAction(title: Strings.homeAlert2Action, style: .default) { [self] (alertAction) in
+            transitionToLandingVC()
         }
         alert.addAction(dismissAction)
         present(alert, animated: true)
@@ -117,10 +118,17 @@ class AccountViewController: UIViewController {
         accountEmailHeadingLabel.hideSkeleton(reloadDataAfter: true, transition: SkeletonTransitionStyle.crossDissolve(0.5))
     }
     
+    func transitionToLandingVC() {
+        let storyboard = UIStoryboard(name: Strings.mainStoryboard, bundle: nil)
+        let landingVC = storyboard.instantiateViewController(identifier: Strings.landingVC)
+        view.window?.rootViewController = landingVC
+        view.window?.makeKeyAndVisible()
+    }
+    
     func getSignedInUserData() {
         user.getSignedInUserData { [self] (error, isUserSignedIn, userData) in
             if error != nil {
-                showErrorFetchingAlert(using: error!.localizedDescription)
+                showErrorFetchingAlert(usingError: true, withErrorMessage: error!)
                 return
             }
             guard let isUserSignedIn = isUserSignedIn else {return}
