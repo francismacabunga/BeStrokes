4e672a3f225e00e4e249ab9a8a448792acc0b82d
Francis <francisnormanmacabunga@gmail.com>
2021-02-02 04:14:51 +0800

    Code cleanup

--- BeStrokes/Controller/Users/TabBarViewController.swift
+++ BeStrokes/Controller/Users/TabBarViewController.swift
@@ -16,14 +16,13 @@ class TabBarViewController: UITabBarController {
         
         UITabBar.appearance().barTintColor = #colorLiteral(red: 0.9529411765, green: 0.9529411765, blue: 0.9647058824, alpha: 1)
         UITabBar.appearance().tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        if let items = tabBar.items {
-            items[0].title = Strings.homeTabText
-            items[0].image = UIImage(systemName: Strings.tabHomeIcon)
-            items[1].title = Strings.captureTabText
-            items[1].image = UIImage(systemName: Strings.tabCaptureIcon)
-            items[2].title = Strings.accountTabText
-            items[2].image = UIImage(systemName: Strings.tabAccountIcon)
-        }
+        guard let items = tabBar.items else {return}
+        items[0].title = Strings.homeTabText
+        items[0].image = UIImage(systemName: Strings.tabHomeIcon)
+        items[1].title = Strings.captureTabText
+        items[1].image = UIImage(systemName: Strings.tabCaptureIcon)
+        items[2].title = Strings.accountTabText
+        items[2].image = UIImage(systemName: Strings.tabAccountIcon)
         
     }
     
